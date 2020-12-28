b263363e71ade583eaa4bfd54eb017b2759e4c11
AkshayaHaasini <75781475+AkshayaHaasini@users.noreply.github.com>
2020-12-28 18:34:54 +0530

    http://127.0.0.1:8887/

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var wall,thickness;
+var bullet,speed,weight;
+
+
+function setup() {
+  createCanvas( 1600,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background("pink");  
+  drawSprites();
+}
